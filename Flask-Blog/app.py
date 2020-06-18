f05fd61ab8dcd9c01081955474db41b2f052433d
Samuele0 <samuele32130@gmail.com>
2020-06-18 15:07:03 +0200

    initial commit

---
+++ Flask-Blog/app.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020 samuele
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
